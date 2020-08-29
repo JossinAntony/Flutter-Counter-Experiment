7c2cca3fbd9b674fc490e3c0b9730709e3d5c0f9
JossinAntony <jossinde@gmail.com>
2020-08-29 12:32:03 +0200

    counter experiment set up.

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,17 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+
   int _counter = 0;
+  int _counter_2 = 0; // 2nd counter
+
+  int _updateCounter(_counter){
+    setState(() {
+      _counter = _counter + 1;
+      print('_counter in function: ' + _counter.toString());  // not getting updated?
+    });
+    return _counter;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,14 +34,17 @@ class _MyAppState extends State<MyApp> {
                   child: Column(
                     children: <Widget>[
 
-                  Text(_counter.toString()),
+                  Text(_counter_2.toString()), //increment using second counter, does not work
                   SizedBox(height: 20),
 
 
                   RaisedButton(
                     onPressed: () {
                       setState(() {
-                        _counter++;
+                        //_counter++;
+                        _counter_2 = _updateCounter(_counter); // this does not seem to work!
+                        print('_counter in RaisedButton: ' + _counter.toString()); // strange behaviour here!
+                        print('_counter_2 in RaisedButton: ' + _counter_2.toString());// strange behaviour here!
                       });
                     },
                     color: Colors.blue,
