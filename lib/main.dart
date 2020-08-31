7689d01b63f1a7e18cfa8349df5bd5efba80ea60
JossinAntony <jossinde@gmail.com>
2020-08-31 18:19:02 +0200

    global _counter variable updated instead of local variable, this solves the issue.

--- lib/main.dart
+++ lib/main.dart
@@ -10,15 +10,15 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
   int _counter = 0;
   int _counter_2 = 0; // 2nd counter
 
-  int _updateCounter(_counter){
-    setState(() {
-      _counter = _counter + 1;
-      print('_counter in function: ' + _counter.toString());  // not getting updated?
-    });
+  int _updateCounter(int _counter) {
+   print( '_counter before increment in function: ' + _counter.toString());
+    _counter = _counter + 1;
+    print(
+        '_counter in function: ' + _counter.toString()); // not getting updated?
+
     return _counter;
   }
 
@@ -26,34 +26,36 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-          appBar: AppBar(
-            title: Text('Counter'),
-          ),
+            appBar: AppBar(
+              title: Text('Counter'),
+            ),
             body: Container(
                 child: Center(
-                  child: Column(
-                    children: <Widget>[
-
-                  Text(_counter_2.toString()), //increment using second counter, does not work
+              child: Column(
+                children: <Widget>[
+                  Text(_counter_2.toString()),
+                  //increment using second counter, does not work
                   SizedBox(height: 20),
-
 
                   RaisedButton(
                     onPressed: () {
                       setState(() {
                         //_counter++;
-                        _counter_2 = _updateCounter(_counter); // this does not seem to work!
-                        print('_counter in RaisedButton: ' + _counter.toString()); // strange behaviour here!
-                        print('_counter_2 in RaisedButton: ' + _counter_2.toString());// strange behaviour here!
+                        _counter_2  = _updateCounter(
+                            _counter); // this does not seem to work!
+                        _counter = _counter_2;//edit: This updates the global variable and hence solves the issue:)
                       });
+
+                      print('_counter in RaisedButton: ' +
+                          _counter.toString()); // strange behaviour here!
+                      print('_counter_2 in RaisedButton: ' +
+                          _counter_2.toString()); // strange behaviour here!
                     },
                     color: Colors.blue,
                     child: Text('+'),
-
-
                   ),
-                    ],
-                  ),
-                ))));
+                ],
+              ),
+            ))));
   }
 }
